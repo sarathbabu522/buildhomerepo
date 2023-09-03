b583e71ca0221d5fd8a22e3df62e63cc94ad094d
root <root@ip-172-31-63-239.ec2.internal>
2023-09-03 03:06:49 +0000

    homejob

---
+++ src/main/java/homebuildplan/homebuildplan/HomebuildplanApplication.java
@@ -0,0 +1,13 @@
+package homebuildplan.homebuildplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebuildplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebuildplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/homebuildplan/homebuildplan/HomebuildplanApplicationTests.java
@@ -0,0 +1,13 @@
+package homebuildplan.homebuildplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebuildplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
